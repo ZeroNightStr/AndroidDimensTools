32a875aa95f2d601118da2c58474145b464c20b0
ZeroNightStr <zeronightstr@gmail.com>
2017-03-06 11:03:21 +0800

    Add files via upload

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-java -jar DimenTest_fat.jar 640 360 384 400 432 480 540 600 640 720 768 800 960 1080
+java -jar AndroidDeminsTools.jar 640 360 384 400 432 480 540 600 640 720 768 800 960 1080
 
 pause
